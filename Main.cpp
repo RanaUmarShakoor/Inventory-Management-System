015f60fb35a8a82deab5568e227d9e16a19439f0
Rana Umar Shakoor <120050041+RanaUmarShakoor@users.noreply.github.com>
2022-12-22 01:51:48 +0500

    Version 2 (Complete)

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // ********** Global Variables  ********** 
 const int FACULTY_MEMBERS = 56;
 const int MAX_NAME_CHAR = 50;
-const int NUMBER_ITEMS = 50;
+const int NUMBER_ITEMS = 10;
 
 
 // ********** Structures  ********** 
@@ -19,6 +19,7 @@ struct Allocated_to
 
 struct InventoryItem
 {
+	bool isFilled = 0;
 	char Name[MAX_NAME_CHAR];
 	int Item_ID;
 	char category[15];
@@ -32,22 +33,30 @@ struct InventoryItem
 void Name_valid(char arr[MAX_NAME_CHAR]);
 void Item_valid(char arr[MAX_NAME_CHAR]);
 void Category_valid(char arr[15]);
+void File();
+void File_read(InventoryItem item[NUMBER_ITEMS]);
+void File_write(InventoryItem item[NUMBER_ITEMS]);
+
+
 //System's functions
 void Menu();
-void Add_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void View_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void Search_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void Edit_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void Delete_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void Assign_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void Retrieve_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
-void List_Assigned_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]);
+void Add_Item(InventoryItem item[NUMBER_ITEMS]);
+void View_Item(InventoryItem item[NUMBER_ITEMS]);
+void Search_Item(InventoryItem item[NUMBER_ITEMS]);
+void Edit_Item(InventoryItem item[NUMBER_ITEMS]);
+void Delete_Item(InventoryItem item[NUMBER_ITEMS]);
+void Assign_Item(InventoryItem item[NUMBER_ITEMS]);
+void Retrieve_Item(InventoryItem item[NUMBER_ITEMS]);
+void List_Assigned_Item(InventoryItem item[NUMBER_ITEMS]);
 
 int main()
 {
 	bool list_isFilled[NUMBER_ITEMS] = { 0 };
 	InventoryItem* item = new InventoryItem[NUMBER_ITEMS];
 	int choice = 0;
+
+	File();
+	File_read(item);
 
 	cout << "****** Inventory Management System *******" << endl;
 	Menu();
@@ -62,44 +71,44 @@ int main()
 		{
 		case 1:
 		{
-			Add_Item(item, list_isFilled);
+			Add_Item(item);
 			break;
 		}
 		case 2:
 		{
-			View_Item(item, list_isFilled);
+			View_Item(item);
 			break;
 		}
 		case 3:
 		{
-			Search_Item(item, list_isFilled);
+			Search_Item(item);
 			break;
 		}
 		case 4:
 		{
-			Edit_Item(item, list_isFilled);
+			Edit_Item(item);
 			break;
 		}
 
 		case 5:
 		{
-			Delete_Item(item, list_isFilled);
+			Delete_Item(item);
 			break;
 		}
 		case 6:
 		{
-			Assign_Item(item, list_isFilled);
+			Assign_Item(item);
 			break;
 
 		}
 		case 7:
 		{
-			Retrieve_Item(item, list_isFilled);
+			Retrieve_Item(item);
 			break;
 		}
 		case 8:
 		{
-			List_Assigned_Item(item, list_isFilled);
+			List_Assigned_Item(item);
 			break;
 		}
 		case 9:
@@ -115,12 +124,13 @@ int main()
 
 		}
 
+		if(choice != 2 && choice != 3 && choice != 8)
+			File_write(item);
 	}
-
 }
 
 
-
+//****************************************  Functions  ****************************************
 
 //For number validation - Do not accept a character
 int Number_valid()
@@ -208,8 +218,69 @@ void Category_valid(char arr[15])
 	}
 }
 
+
+//File creation or (if exist) do nothing
+void File()
+{
+	ofstream fout("Inventory_item_data.bin", ios::binary | ios::app);
+	if (fout)
+	{
+		fout.close();
+	}
+	else
+	{
+		cout << "File was not opened." << endl;
+	}
+
+}
+
+//File Reading
+void File_read(InventoryItem item[NUMBER_ITEMS])
+{
+	ifstream fin("Inventory_item_data.bin", ios::binary);
+	if (fin)
+	{
+
+		fin.seekg(0, ios::end);
+		int size = fin.tellg();
+		fin.seekg(0, ios::beg);
+		for (int i = 0; i <= size / sizeof(item[0]); i++)
+		{
+			fin.read(reinterpret_cast<char*>(&item[i]), sizeof(item[0]));
+		}
+		fin.clear();
+		fin.close();
+	}
+	else
+	{
+		cout << "File was not opened. Couldn't read it." << endl;
+	}
+
+}
+
+//File Writing
+void File_write(InventoryItem item[NUMBER_ITEMS])
+{
+	ofstream fout("Inventory_item_data.bin", ios::binary);
+	if (fout)
+	{
+
+		fout.seekp(0, ios::end);
+		int size = fout.tellp();
+		for (int i = 0; i < NUMBER_ITEMS; i++)
+		{
+			fout.write(reinterpret_cast<char*>(&item[i]), sizeof(item[0]));
+		}
+		fout.close();
+	}
+	else
+	{
+		cout << "File was not opened. Couldn't write in it." << endl;
+	}
+}
+
 //To search an item using item_ID
-int Search_ID(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+int Search_ID(InventoryItem item[NUMBER_ITEMS])
 {
 	int userID = 0, index = 0;
 	bool isFound = 0;
@@ -224,7 +295,7 @@ int Search_ID(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]
 		//Checking if the item of entered ID exist
 		for (int i = 0; i < NUMBER_ITEMS; i++)
 		{
-			if (list_isFilled[i] == 1)
+			if (item[i].isFilled == 1)
 			{
 				if (item[i].Item_ID == userID)
 				{
@@ -264,16 +335,15 @@ void Menu()
 }
 
 //Add inventory item
-void Add_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void Add_Item(InventoryItem item[NUMBER_ITEMS])
 {
-	int count = 0;
-
+	int index = 0;
 	//Checks the list to find an empty node in the array
 	for (int i = 0; i < FACULTY_MEMBERS; i++)
 	{
-		if (list_isFilled[i] == 0)
+		if (item[i].isFilled == 0)
 		{
-			count = i;
+			index = i;
 			break;
 		}
 	}
@@ -281,26 +351,47 @@ void Add_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]
 	cin.ignore(10000, '\n');
 
 	//Taking input info for the date
-	Name_valid(item[count].Name);
+	Item_valid(item[index].Name);
 
 	//Taking valid input for Item ID if it is's a number above 0
 	while (1)
 	{
+		int user_ID;
 		cout << "Enter item_ID: ";
-		item[count].Item_ID = Number_valid();
-		if (item[count].Item_ID > 0)
+		user_ID = Number_valid();
+
+		bool isDuplicate = 0;
+		for (int i = 0; i < NUMBER_ITEMS; i++)
 		{
+			if (user_ID == item[i].Item_ID)
+			{
+				isDuplicate = 1;
+				break;
+			}
+		}
+
+		if (isDuplicate)
+		{
+			cout << "Item with entered ID already exist. Please enter a new ID." << endl;
+		}
+		else if (user_ID > 0)
+		{
+			item[index].Item_ID = user_ID;
 			break;
 		}
-		cout << "Invalid item_ID entered. Please re-enter." << endl;
+		else
+		{
+			cout << "Invalid item_ID entered. Please re-enter." << endl;
+		}
+
 	}
 
 	//Taking input for item_count only if it's not negative number
 	while (1)
 	{
 		cout << "Enter Number of items: ";
-		item[count].Item_count = Number_valid();
-		if (item[count].Item_count >= 0)
+		item[index].Item_count = Number_valid();
+		if (item[index].Item_count >= 0)
 		{
 			break;
 		}
@@ -309,15 +400,15 @@ void Add_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS]
 
 
 	//Taking input for item_category
-	Category_valid(item[count].category);
+	Category_valid(item[index].category);
 
 	//Updating the items list
-	list_isFilled[count] = 1;
-	cout << "Item added." << endl << endl;
+	item[index].isFilled = 1;
+	cout << "Item Added. " << endl << endl;
 }
 
 //View inventory items
-void View_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void View_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	cout << setw(7) << "ID" << ' ' << setw(15) << "Category" << '\t' << ' '
 		<< left << setw(50) << "Item Name" << right << " " << setw(15) << "Count" << endl
@@ -325,10 +416,10 @@ void View_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS
 	for (int i = 0; i < NUMBER_ITEMS; i++)
 	{
 		//CHecking the list to find filled space and displaying that item
-		if (list_isFilled[i] == 1)
+		if (item[i].isFilled == 1)
 		{
 			cout << setw(7) << item[i].Item_ID << '|' << setw(15) << item[i].category << '\t' << '|'
-				<< left << setw(50) << item[i].Name << right << '|' << setw(15) << item[i].Item_count << endl << endl;
+				<< left << setw(50) << item[i].Name << right << '|' << setw(15) << item[i].Item_count << endl;
 		}
 	}
 
@@ -336,7 +427,7 @@ void View_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS
 }
 
 //Search inventory items
-void Search_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void Search_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	char user_name[MAX_NAME_CHAR];
 	cin.ignore();
@@ -346,7 +437,7 @@ void Search_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITE
 	for (int i = 0; i < NUMBER_ITEMS; i++)
 	{
 		//Checking if the node is Filled
-		if (list_isFilled[i])
+		if (item[i].isFilled)
 		{
 			//Checking if the Item exist and displaying it
 			if (strcmp(user_name, item[i].Name) == 0)
@@ -365,12 +456,12 @@ void Search_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITE
 }
 
 //Edit inventory items
-void Edit_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void Edit_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	//GEtting input for ID from the user and validating  it
-	int index = Search_ID(item, list_isFilled);
+	int index = Search_ID(item);
 
-	cout << "Enter NEW data" << endl;
+	cout << endl << "Enter NEW data" << endl;
 	Item_valid(item[index].Name);
 
 	//Taking valid input for Item ID
@@ -405,14 +496,14 @@ void Edit_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS
 }
 
 //Delete inventory items
-void Delete_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void Delete_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	int index = 0;
 	//Getting ID from the user and validating it
-	index = Search_ID(item, list_isFilled);
+	index = Search_ID(item);
 
 	//DELETING
-	list_isFilled[index] = 0;
+	item[index].isFilled = 0;
 	item[index].Item_ID = 0;
 	item[index].allocatedItems = 0;
 
@@ -420,13 +511,13 @@ void Delete_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITE
 }
 
 //Assign inventory item
-void Assign_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void Assign_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	bool isAssignee_Available = 0;
 	int index = 0, assignee = -1, allocatedItems = 0;
 
 	//Getting ID from the user and validating it
-	index = Search_ID(item, list_isFilled);
+	index = Search_ID(item);
 
 	//Checking if any item is in STOCK (if no: exiting)
 	if (item[index].Item_count <= 0)
@@ -487,7 +578,7 @@ void Assign_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITE
 			cout << "Invalid number of items entered. Please re-enter." << endl;
 		}
 
-		cout << "(" << (allocatedItems) << ") items assigned to " << item[index].assigned_to[assignee].name << endl << endl;
+		cout << "(" << (allocatedItems) << ')' << item[index].Name << " is assigned to " << item[index].assigned_to[assignee].name << endl << endl;
 	}
 	else
 	{
@@ -498,14 +589,14 @@ void Assign_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITE
 }
 
 //Retrieving items from the Assignees
-void Retrieve_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void Retrieve_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	int index = 0, assignee = 0, returnedItems = 0;
 	char userName[MAX_NAME_CHAR];
 	bool isFound = 0;
 
 	//Getting ID from the user and validating it
-	index = Search_ID(item, list_isFilled);
+	index = Search_ID(item);
 
 	//Checking if the item is assigned or not
 	if (item[index].allocatedItems <= 0)
@@ -565,21 +656,21 @@ void Retrieve_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_I
 }
 
 //Displaying the list of Assignees of a Specific item
-void List_Assigned_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUMBER_ITEMS])
+void List_Assigned_Item(InventoryItem item[NUMBER_ITEMS])
 {
 	int index = 0;
 	//Getting ID from the user and validating it
-	index = Search_ID(item, list_isFilled);
+	index = Search_ID(item);
 
 	//Checking if any item is assigned
 	if (item[index].allocatedItems <= 0)
 	{
-		cout << "No item is ASSIGNED." << endl;
+		cout  << endl << "No item is ASSIGNED." << endl;
 		return;
 	}
 
 	//Displaying the list of persons who borrowed the item
-	cout << left << setw(50) << "Name" << setw(8) << "Number of Items" << endl;
+	cout  << endl << left << setw(50) << "Name" << setw(8) << "Number of Items" << endl;
 	cout << setfill('-') << setw(65) << "-" << setfill(' ') << endl;
 	for (int i = 0; i < FACULTY_MEMBERS; i++)
 	{
@@ -588,4 +679,5 @@ void List_Assigned_Item(InventoryItem item[NUMBER_ITEMS], bool list_isFilled[NUM
 			cout << setw(50) << item[index].assigned_to[i].name << setw(8) << item[index].assigned_to[i].allocated_items << endl;
 		}
 	}
+	cout << endl;
 }
